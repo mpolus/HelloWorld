1837e2a88321d762d5d13a4a7f056e89ea695ee3
Malaika Poluscar <polusmal001@warren.k12.in.us>
2024-08-05 12:52:48 -0400

    live love laugh

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class HelloWorld {
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.print("Hello");
+        System.out.print(" ");
+        System.out.print("Software");
+        System.out.print(" ");
+        System.out.print("Development");
+    }
 }
